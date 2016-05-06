0377b7de959d908df7b66cb60f97d7b2e2946e5f
williammlu <lu.william.m@gmail.com>
2016-05-05 20:16:30 -0700

    added files for mouse explore run and speed run phase

---
+++ scripts/speedRun.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from mouse import Mouse
+from board import Board
+from time import sleep
+from controller import Controller
+import sys
+import rospy
+
+x = 0
+y = 0
+direction = 1
+
+# can run using python 1 2 3 where it starts at (x,y), direction is left
+# if(len(sys.argv) == 1 ):
+    # file = sys.arv[0]
+
+rospy.init_node('maze_solver')
+board = Board()
+board.read("exploredBoard.txt")
+mouse = Mouse(x,y,direction,board,1)
+
+mouse.printBoard()
+
+
+
+mouse.floodFillToGoal()
+# runs true shortest path because board is explored already
